b754d1463f57f50d4303d67bd7d7f68a3ff1e233
panoptus@gmail.com <panoptus@gmail.com@a3451407-9e38-582e-5c96-78cc79d18a27>
2013-02-11 18:08:25 +0000

    Added error when img or thumb dir is not writable

--- trunk/engine.php
+++ trunk/engine.php
@@ -1,12 +1,20 @@
 <?php
 include_once 'hal.php';
 
+$uploaddir=$config['uploaddir'].$config['current_path'];
+$thumbdir=$config['thumbdir'].$config['current_path'];
+
 if (!is_dir ($config['working_thumb_dir']))
 	mkdir ($config['working_thumb_dir'], 0777, true);
 
-if (!is_dir ($config['uploaddir'].$config['current_path']))
-	mkdir ($config['uploaddir'].$config['current_path'], 0777, true);
+if (!is_dir ($uploaddir))
+	mkdir ($uploaddir, 0777, true);
 
-if (!is_dir ($config['thumbdir'].$config['current_path']))
-	mkdir ($config['thumbdir'].$config['current_path'], 0777, true);
+if (!is_dir ($thumbdir))
+	mkdir ($thumbdir, 0777, true);
+
+if (!is_writable($uploaddir))
+	$error[]="Ошибка! Директория ".$uploaddir." недоступна для записи";
+if (!is_writable($config['thumbdir'].$config['current_path']))
+	$error[]="Ошибка! Директория ".$thumbdir." недоступна для записи";
 ?>
