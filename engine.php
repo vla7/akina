6d8471ad1666bf7043e26f7ff22a3b29f6c319c4
vla7 <vla7@users.noreply.github.com>
2015-08-29 20:20:21 +0300

    Some little fixes for clear initial install

--- engine.php
+++ engine.php
@@ -1,20 +1,5 @@
 <?php
 include_once 'hal.php';
+//временно пустует. Позже надо всю логику из config.php вынести сюда, оставил в конфиге только переменные
 
-$uploaddir=$config['uploaddir'].$config['current_path'];
-$thumbdir=$config['thumbdir'].$config['current_path'];
-
-if (!is_dir ($config['working_thumb_dir']))
-	mkdir ($config['working_thumb_dir'], 0777, true);
-
-if (!is_dir ($uploaddir))
-	mkdir ($uploaddir, 0777, true);
-
-if (!is_dir ($thumbdir))
-	mkdir ($thumbdir, 0777, true);
-
-if (!is_writable($uploaddir))
-	$error[]="Ошибка! Директория ".$uploaddir." недоступна для записи";
-if (!is_writable($thumbdir))
-	$error[]="Ошибка! Директория ".$thumbdir." недоступна для записи";
 ?>
